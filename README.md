e37f7b631d39fa1c182a26bdbbe022b86586dca3
dinh0012 <dinh020304@gmail.com>
2020-01-01 21:33:41 +0700

    add check code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # docker
+./run.sh up
+
+# initial test
+docker exec -it dokcer_poilerplate_php sh -c "sh ../php-ci/bin/initialize.sh"
+
+# make check
+docker exec -it dokcer_poilerplate_php sh -c "make check"
--- run.sh
+++ run.sh
@@ -8,15 +8,30 @@ if ! test -f "$ENV"; then
 fi
 
 source $ENV
+
+PRE_PUSH=./.git/hooks/pre-push
+if ! test -L "$PRE_PUSH"; then
+   echo "#### INIT FOR PHP-CI ############"
+   ln -s ./pre-push-hooks.sh .git/hooks/pre-push
+fi
+
+MAKE_FILE=./Makefile
+MAKE_FILE_SRC=src/Makefile
+if ! test -f "MAKE_FILE_SRC"; then
+  echo "#### INIT FOR PHP-CI ############"
+  cp $MAKE_FILE $MAKE_FILE_SRC
+fi
+
 SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT")
 PROJECT_DIR="$(dirname $(dirname $DIR))"
 DOCKER_FILES="-f docker-compose.yml \
 -f ./services/postgres.yml"
 #-f ./services/mysql.yml"
-
-
+export PROJECT_DIR=$PROJECT_DIR
 # Create global network
 docker network inspect "${APP_NAME}_network" &>/dev/null || docker network create --driver bridge "${APP_NAME}_network"
 #run docker-compose
 docker-compose $DOCKER_FILES "$@"
+docker exec -it "$APP_NAME"_php sh -c "sh ../php-ci/bin/initialize.sh"
+
