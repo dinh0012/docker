9d73b7471fbf3d985d3282701960d041e030c23b
dinh0012 <dinh020304@gmail.com>
2020-01-01 20:53:06 +0700

    add postgresql

--- run.sh
+++ run.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source ./.env
-SCRIPT=`readlink -f $0`
-DIR=`dirname $SCRIPT`
+ENV=./.env
+ENV_EXAMPALE=./.env.example
+
+#copy file ENV
+if ! test -f "$ENV"; then
+    cp ${ENV_EXAMPALE} ${ENV}
+fi
+
+source $ENV
+SCRIPT=$(readlink -f "$0")
+DIR=$(dirname "$SCRIPT")
 PROJECT_DIR="$(dirname $(dirname $DIR))"
-DOCKER_FILES="-f docker-compose.yml"
+DOCKER_FILES="-f docker-compose.yml \
+-f ./services/postgres.yml"
+#-f ./services/mysql.yml"
+
 
 # Create global network
 docker network inspect "${APP_NAME}_network" &>/dev/null || docker network create --driver bridge "${APP_NAME}_network"
-
-export PROJECT_DIR=$PROJECT_DIR
-
 #run docker-compose
-docker-compose $DOCKER_FILES $@
+docker-compose $DOCKER_FILES "$@"
--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,16 @@
 <?php
-echo 2222;
+$servername = getenv('DB_HOST');
+$dbName = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+try {
+  //  $conn = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
+    $conn = new PDO("pgsql:host=$servername;dbname=$dbName", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
