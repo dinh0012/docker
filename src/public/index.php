de42283ccda05cfb32254c154f9feb580ebeb822
dinhnv <dinhnv@vnext.com.vn>
2020-01-02 10:57:07 +0700

    add memcached

--- src/public/index.php
+++ src/public/index.php
@@ -8,9 +8,14 @@ try {
     $conn = new PDO("pgsql:host=$servername;dbname=$dbName", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    echo "Database is Connected successfully";
 }
 catch(PDOException $e)
 {
     echo "Connection failed: " . $e->getMessage();
 }
+
+$mc = new Memcached('mc');
+$mc->setOption(Memcached::OPT_LIBKETAMA_COMPATIBLE, true);
+$check = $mc->addServer(getenv('DOMAIN_MEMCACHE'), getenv('MEMCACHE_PORT_DOCKER'));
+echo "<br /> Memcached is connected: $check";
